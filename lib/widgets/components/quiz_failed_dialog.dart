8ba84671d033c3d07b6621a2c1eaf0703122e2ef
SEED0228 <naokitane6828@gmail.com>
2023-10-01 05:15:58 +0900

    ツイート共有機能を追加

--- lib/widgets/components/quiz_failed_dialog.dart
+++ lib/widgets/components/quiz_failed_dialog.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:localingo/enitity/answer.dart';
 import 'package:localingo/enitity/question.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class QuizFailedDialog extends StatelessWidget {
   final Question question;
@@ -18,6 +20,17 @@ class QuizFailedDialog extends StatelessWidget {
         style: const TextStyle(fontSize: 20),
       ),
       actions: <Widget>[
+        GestureDetector(
+          child: const Icon(FontAwesomeIcons.twitter),
+          onTap: () async {
+            String text =
+                '#Localingo\n残念！\n正解は「${question.answers.where((Answer answer) => answer.correct).toList()[0].answer}」です！\nあなたの正当数は$scoreでした。';
+
+            await launchUrl(
+                Uri.https('twitter.com', '/intent/tweet', {'text': text}),
+                mode: LaunchMode.externalApplication);
+          },
+        ),
         GestureDetector(
           child: const Text(
             'はい',
