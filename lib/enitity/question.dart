00334d72dd03dd95f972d909df11c06c30a4c748
SEED0228 <naokitane6828@gmail.com>
2023-10-01 01:26:54 +0900

    質問もチャットに含められるよう修正

--- lib/enitity/question.dart
+++ lib/enitity/question.dart
@@ -1,11 +1,12 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import '../ui_models/chat_element.dart';
 import 'answer.dart';
 
 part 'question.freezed.dart';
 part 'question.g.dart';
 
 @freezed
-class Question with _$Question {
+class Question with _$Question implements ChatElement {
   const factory Question({required String question, required List<Answer> answers, required String explanation}) = _Question;
 
   factory Question.fromJson(Map<String, dynamic> json) => _$QuestionFromJson(json);
